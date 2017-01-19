9ba8fa6d286711518d3a534be04ad30274e59e59
Regulo Leonardi <regulo.leonardi@gmail.com>
2017-01-19 15:21:00 +0100

    Initial Commit

---
+++ MapGameApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MapGameApp
+//
+//  Created by dedam on 19/1/17.
+//  Copyright © 2017 dedam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MapGameApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MapGameApp
+//
+//  Created by dedam on 19/1/17.
+//  Copyright © 2017 dedam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
